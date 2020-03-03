8371aeca7d2631dcbfd8ee8c2b0654925f4e16f0
Shanbora <chanbora@logilink.fr>
2020-03-03 14:31:58 +0100

    responsable set contact to null

--- lib/ImmobilierBundle/Entity/ShResponsable.php
+++ lib/ImmobilierBundle/Entity/ShResponsable.php
@@ -19,7 +19,7 @@ class ShResponsable
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $contact;
 
